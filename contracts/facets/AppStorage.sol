9ac83372f4c90aee7dba5b172b9a3a2e4872fb47
“Chidubem <chidubem@rawdev.com.ng>
2023-02-14 14:22:29 +0100

    Salary

--- contracts/facets/AppStorage.sol
+++ contracts/facets/AppStorage.sol
@@ -4,6 +4,7 @@ pragma solidity >=0.4.22 <0.9.0;
 struct AppStorage {
   mapping (address=>bool)  pythia;
   mapping(address => address[]) myReferrals;
+  mapping(address => uint) staffId;
   
 
 }
--- contracts/facets/SalaryFacet.sol
+++ contracts/facets/SalaryFacet.sol
@@ -7,22 +7,50 @@ import "../libraries/SafeMath.sol";
 import "./AppStorage.sol";
 
 contract SalaryFacet {
-struct Workers{
+AppStorage internal s;
+using SafeMath for uint256;
+using SafeDecimalMath for uint;
+struct Staffs{
     uint salary;
     address account;
     string name;
-   }   
-  Workers[] workers;
-  
+    uint nextPayDate;
+   }  
+  Staffs[] staffs;
+  event StaffEnrolled(
+    uint salary,
+    address account,
+    string name,
+    uint staffId,
+    uint time);
  
-  function Addworkers(string[] calldata _name, address[] calldata _account, uint[] calldata _salary) external onlyOwner{
+  function AddStaffs(string[] calldata _name, address[] calldata _account, uint[] calldata _salary) external onlyOwner{
     require(_name.length == _account.length, "Invalid parameters");
     require(_salary.length == _account.length, "Invalid parameters");
     for (uint256 i; i < _name.length; i++) {
-      
-   
+        Staffs memory staff = Staffs({
+        salary: _salary[i],
+        account: _account[i],
+        name: _name[i],
+        nextPayDate: block.timestamp.add(30 days)
+        });
+        staffs.push(staff);
+        uint256 staffId = staffs.length - 1;
+        s.staffId[_account[i]] = staffId;
+        emit StaffEnrolled(_salary[i], _account[i], _name[i], staffId, block.timestamp);
+
         }
   }
+
+  function PayStaffs() external onlyOwner{
+   
+    
+      for (uint256 i = 0; i < staffs.length; i++) {
+          
+    }
+  }
+
+
 
 modifier onlyOwner{
         require(_msgSender() == LibDiamond.contractOwner(), "Access denied, Only owner is allowed!");
